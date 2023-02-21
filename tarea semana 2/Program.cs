5a1e01363f501d854f9d545717fcbc7b80bf4cc9
ManuelGrijalva <mgrijalvatenas@gmail.com>
2023-02-21 16:18:54 -0600

    first commit

---
+++ tarea semana 2/Program.cs
@@ -0,0 +1,6 @@
+﻿// ejercicio 1
+// edad//
+byte edad;
+Console.WriteLine("Dime tu edad?");
+edad = Convert.ToByte(Console.ReadLine());
+Console.WriteLine("No puedo creer que tengas : " + edad + " Años.");
