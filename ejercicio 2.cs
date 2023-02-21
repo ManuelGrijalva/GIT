cce69b053bc81b21d84d61212ac49358fb0602c6
ManuelGrijalva <mgrijalvatenas@gmail.com>
2023-02-21 16:34:27 -0600

    first commit

---
+++ ejercicio 2.cs
@@ -0,0 +1,13 @@
+﻿//multiplicacion//
+
+byte digito1, digito2;
+
+ushort resultado;
+
+Console.WriteLine("Ingresa dos Digitos para Multiplicar:");
+Console.WriteLine("digito1:");
+digito1 = Convert.ToByte(Console.ReadLine());
+Console.WriteLine("digito2:");
+digito2 = Convert.ToByte(Console.ReadLine());
+resultado = Convert.ToUInt16(digito1 * digito2);
+Console.WriteLine("Resultado de la operacion:" + resultado);
